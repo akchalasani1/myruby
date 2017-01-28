5698fdbca2914f37fc5a6d3afeec53bc60661160
Anil Chalasani <anil@Anils-MacBook-Air.local>
2017-01-27 16:51:43 -0800

    added calculate.rb

---
+++ calculate.rb
@@ -0,0 +1,13 @@
+
+# below is a string with double codes""
+puts "do you think 2 + 39 + 50 is greater than 100? "
+# below is not a string
+puts 2 + 39 + 50 > 100
+
+# below is a strings with double codes " " 
+puts "Is 33 + 44 greater 99 + 5 - 4  true or false? #{33 + 44 > 99 + 5 - 4}"
+
+puts "Let's check if 33 is greater than 34: #{33 > 34}"
+puts "Let's check if 33 is greater or equal to 34: #{33 >= 34}"
+puts "Let's check if 33 is less than or equal to 34: #{33 <= 34}"
+puts "Let's check if 33 is equal to 34: #{33 == 34}"
