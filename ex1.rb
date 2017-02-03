3926d9d9dd8c11662ef9fa05d936adb0ada0e26c
Anil Chalasani <anilchalasani@Anils-MacBook-Pro.local>
2017-02-03 12:53:49 -0800

    “comment”

--- ex1.rb
+++ ex1.rb
@@ -5,7 +5,7 @@
 #puts "I like typing this."
 #puts "This is fun."
 #puts "Yay! Printing."
-#puts "I'd much rather you 'not'."
+puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
 puts "This is the 1st new line."
 puts 'This is the 2nd new line.'
